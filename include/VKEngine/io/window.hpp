684c3c7957bb8e3feb467f9224345a0796d604ee
Mrpupsik228 <cubertank567u@gmail.com>
2025-06-02 19:50:41 +0200

    Added Window class

---
+++ include/VKEngine/io/window.hpp
@@ -0,0 +1,22 @@
+#pragma once
+
+#define GLFW_INCLUDE_NONE
+
+#include <vulkan/vulkan.hpp>
+#include <GLFW/glfw3.h>
+#include <string>
+
+class Window {
+public:
+    Window(int width, int height, std::string title);
+    ~Window();
+    
+private:
+    int width;
+    int height;
+    std::string title;
+    
+    GLFWwindow* window;
+
+    void initVulkan();
+};
---
+++ src/io/window.cpp
@@ -0,0 +1,21 @@
+#include <VKEngine/io/window.hpp>
+
+Window::Window(int width, int height, std::string title)
+    : width(width), height(height), title(title) {
+    
+    if (!glfwInit()) {
+        throw std::runtime_error("Failed to initialize GLFW");
+    }
+    glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+
+    window = glfwCreateWindow(width, height, title.c_str(), nullptr, nullptr);
+
+}
+
+Window::~Window() {
+    if (window) {
+        glfwDestroyWindow(window);
+    }
+    
+    glfwTerminate();
+}
